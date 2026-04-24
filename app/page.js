3ee664aa61753f39e362c0b79c1cb23e9d25d7df
BuyersClubLeon <leon.vintage.clothing@gmail.com>
2026-04-25 02:28:42 +0900

    Fix trailing JSX closing tags in page

--- app/page.js
+++ app/page.js
@@ -3097,9 +3097,6 @@ const replyQuestionPreview = getResultPreviewText(
                   </div>
                 )}
 
-          </div>
-        </div>
-      </div>
 
       <style>{`@keyframes spin { from { transform: rotate(0deg); } to { transform: rotate(360deg); } }`}</style>
     </div>
